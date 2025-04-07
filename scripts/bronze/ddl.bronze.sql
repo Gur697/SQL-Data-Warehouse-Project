72a84000e08c2e523ec9fbf49286b7a2f79fd23e
Gurpal Singh <68579857+Gur697@users.noreply.github.com>
2025-04-07 11:19:34 +0530

    Update ddl.bronze.sql

--- scripts/bronze/ddl.bronze.sql
+++ scripts/bronze/ddl.bronze.sql
@@ -15,9 +15,9 @@ BEGIN TRY
     CREATE TABLE bronze.crm_cust_info (
         cst_id INT,
         cst_key NVARCHAR(50),
-        cst_fisrtname NVARCHAR(50),
+        cst_firstname NVARCHAR(50),
         cst_lastname NVARCHAR(50),
-        cst_material_status NVARCHAR(50),
+        cst_marital_status NVARCHAR(50),
         cst_gndr NVARCHAR(50),
         cst_create_date DATE
     );
